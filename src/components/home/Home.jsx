8d6fcf6d1fc0f32470003bd2d2bc0d4cd1d761cc
YonyChaparro <ychaparro@unal.edu.co>
2024-08-10 20:16:33 -0500

    fixesNavbar&&Colapse

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,11 @@
 import { NavbarHome } from "./Navbar"
-import Image from 'react-bootstrap/Image';
 
 
 
 export const Home = () => {
     return (
         <>
-            <NavbarHome />            
+            <NavbarHome />
         </>
     )
 }
--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -3,15 +3,15 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export const NavbarHome = () => {
   return (
-    <Navbar id="navbar nav-transparent" expand="lg">
-      <Container>
+    <Navbar id="navbar" expand="lg">
+      <Container className="d-flex justify-content-center" >
         <Navbar.Brand href="#inicio" >
           <img src="/src/assets/Logos/Letra alma condor en transparente (1).png" alt="Alma Condor logo" className="logoAlmaCondor" />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="d-flex justify-content-center">
-          <Nav>
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center" >
+          <Nav className="d-flex align-items-center">
             <Nav.Link href="#presentaciones">PRESENTACIONES</Nav.Link>
             <Nav.Link href="#galeria">GALERÍA</Nav.Link>
             <Nav.Link href="#blog-andino">BLOG ANDINO</Nav.Link>
